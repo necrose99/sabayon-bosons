157297476344b880d2796d4824458863410400d9
mudler <mudler@dark-lab.net>
2015-11-12 17:58:24 +0100

    [circle] added deploy, testing

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+sudo apt-get install -y rsync
+mkdir -p ~/.ssh/ || true
+echo "$DEPLOY_PRIVKEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+rsync -avp -e "ssh -p $DEPLOY_PORT -i ~/.ssh/id_rsa" $CIRCLECI_ARTIFACTS/* $DEPLOY_SERVER
