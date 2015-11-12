e99df769b0c1c7c7e9e0e4e990cb679eecf2aaf2
mudler <mudler@dark-lab.net>
2015-11-12 16:39:40 +0100

    [circle] removing arm, binfmt is not working

--- setup.sh
+++ setup.sh
@@ -5,19 +5,6 @@ set -o errexit
 wget 'https://github.com/mudler/boson/releases/download/v0.2.3/boson_linux_amd64' \
           -O ~/boson && chmod +x ~/boson
 
-# Trying to able to use arm with qemu-arm-static
-
-# load the binfmt_misc module
-if [ ! -d /proc/sys/fs/binfmt_misc ]; then
-  sudo /sbin/modprobe binfmt_misc
-fi
-
-sudo apt-get install -y binfmt-support
-
-sudo echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-binfmt:' > /proc/sys/fs/binfmt_misc/register
-sudo wget 'https://github.com/Sabayon/docker-base-armhfp/raw/master/ext/qemu-arm-static' \
-          -O /usr/bin/qemu-arm-binfmt && chmod +x /usr/bin/qemu-arm-binfmt
-
 sudo wget 'https://raw.githubusercontent.com/Sabayon/docker-builder-amd64/master/script/builder' \
           -O /builder && chmod +x /builder
 
