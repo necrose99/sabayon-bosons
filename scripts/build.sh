704efbb237f3230d18d862ab0a8630749f9120eb
mudler <mudler@dark-lab.net>
2015-11-12 19:50:12 +0100

    [script] more verbosity

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,22 @@ set -o errexit
 
 
 if [[ ! -e ~/.bosonstate ]]; then mkdir -p ~/.bosonstate; fi
+echo ">> Starting build of $1"
+echo ">> Extrapolating last commit, and saving it in $2"
+
 curl -s $1 | \
         grep '"sha": "' | \
         sed -n '1p' | \
         sed 's/  "sha": "//g' | \
         sed 's/",//g'  > $2
-if [[ ! -e $3 ]]; then cp -rfv $2 $3; fi
+
+if [[ ! -e $3 ]]; then
+  cp -rfv $2 $3
+  echo ">> There wasn't a previous commit, saving it to $3"
+fi
+
+echo ">> Starting build from "
+cat $3
 
 #It's return value for now is not used
 BOSON_FROM=$(cat $3) ~/boson -c $4 || true
