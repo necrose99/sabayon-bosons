6fdc846a7221eec45d0ab10e62baf439d5174779
mudler <mudler@dark-lab.net>
2015-11-12 19:42:07 +0100

    cleanup, lint

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -o nounset
-set -o errexit
-
-
-./get_last_commit.sh $1 $2 $3
-#It's return value for now is not used
-BOSON_FROM=$(cat $3) ~/boson -c $4 || true
-cp -rfv $2 $3
--- scripts/build.sh
+++ scripts/build.sh
@@ -10,3 +10,7 @@ curl -s $1 | \
         sed 's/  "sha": "//g' | \
         sed 's/",//g'  > $2
 if [[ ! -e $3 ]]; then cp -rfv $2 $3; fi
+
+#It's return value for now is not used
+BOSON_FROM=$(cat $3) ~/boson -c $4 || true
+cp -rfv $2 $3
