86ab33ca4e411b7b7b9ecceba87f88bf498ae01e
mudler <mudler@dark-lab.net>
2015-11-12 16:42:18 +0100

    [setup] removing /builder, it's already included in docker build images

--- setup.sh
+++ setup.sh
@@ -5,8 +5,5 @@ set -o errexit
 wget 'https://github.com/mudler/boson/releases/download/v0.2.3/boson_linux_amd64' \
           -O ~/boson && chmod +x ~/boson
 
-sudo wget 'https://raw.githubusercontent.com/Sabayon/docker-builder-amd64/master/script/builder' \
-          -O /builder && chmod +x /builder
-
 sudo mkdir -p /log_dir
 sudo mkdir -p /artifacts_dir
