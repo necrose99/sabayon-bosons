a2afa0914d76655c4456d18c513df12308b9f735
mudler <mudler@dark-lab.net>
2015-11-12 15:58:12 +0100

    trying with armhfp

--- setup.sh
+++ setup.sh
@@ -4,5 +4,11 @@ set -o errexit
 
 wget 'https://github.com/mudler/boson/releases/download/v0.2.3/boson_linux_amd64' \
           -O ~/boson && chmod +x ~/boson
+
+# Trying to able to use arm with qemu-arm-static
+sudo echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-binfmt:' > /proc/sys/fs/binfmt_misc/register
+sudo wget 'https://github.com/Sabayon/docker-base-armhfp/raw/master/ext/qemu-arm-static' \
+          -O /usr/bin/qemu-arm-binfmt && chmod +x /usr/bin/qemu-arm-binfmt
+
 sudo mkdir -p /log_dir
 sudo mkdir -p /artifacts_dir
