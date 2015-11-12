6bfaea4fa89767245b697e14885a3019b0bb275b
mudler <mudler@dark-lab.net>
2015-11-12 14:55:22 +0100

    [setup] updating boson version

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -o nounset
 set -o errexit
 
-wget 'https://github.com/mudler/boson/releases/download/v0.2.2/boson_linux_amd64' \
+wget 'https://github.com/mudler/boson/releases/download/v0.2.3/boson_linux_amd64' \
           -O ~/boson && chmod +x ~/boson
 sudo mkdir -p /log_dir
 sudo mkdir -p /artifacts_dir
