9fec2fb5e4cc0d691b1370c5fec70e0c7a99f7a6
mudler <mudler@dark-lab.net>
2015-11-12 16:32:49 +0100

    testing - armhfp

--- setup.sh
+++ setup.sh
@@ -12,8 +12,9 @@ if [ ! -d /proc/sys/fs/binfmt_misc ]; then
   sudo /sbin/modprobe binfmt_misc
 fi
 if [ ! -f /proc/sys/fs/binfmt_misc/register ]; then
-  sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
+  #sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 fi
+sudo apt-get install -y binfmt-support
 
 sudo echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-binfmt:' > /proc/sys/fs/binfmt_misc/register
 sudo wget 'https://github.com/Sabayon/docker-base-armhfp/raw/master/ext/qemu-arm-static' \
