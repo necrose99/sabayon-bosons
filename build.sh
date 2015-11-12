8831acb2d39f0a77c68b0adcda1cc8e743ff064b
mudler <mudler@dark-lab.net>
2015-11-12 16:53:01 +0100

    ignoring boson return value for now

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -o errexit
 
 
 ./get_last_commit.sh $1 $2 $3
-BOSON_FROM=$(cat $3) ~/boson -c $4
+#It's return value for now is not used
+BOSON_FROM=$(cat $3) ~/boson -c $4 || true
 cp -rfv $2 $3
